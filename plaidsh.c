11d820e5d89df0554bce4214e8145893ccb00178
ngirimana <schadran@andrew.cmu.edu>
2023-12-10 11:29:42 +0200

    adding cd functionalities

--- plaidsh.c
+++ plaidsh.c
@@ -1,7 +1,3 @@
-/*
- *
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,6 +10,25 @@
 #include "tokenize.h"
 #include "parse.h"
 #include "pipeline.h"
+
+// Function to remove backslashes from a string
+void remove_backslashes(char *str)
+{
+    int i, j;
+    for (i = 0, j = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != '\\')
+        {
+            str[j++] = str[i];
+        }
+        else if (str[i + 1] == '\\')
+        {
+            str[j++] = '\\';
+            i++;
+        }
+    }
+    str[j] = '\0';
+}
 
 int main(int argc, char *argv[])
 {
@@ -39,28 +54,43 @@ int main(int argc, char *argv[])
             char *pipeline_string = GetPipelineString(pipeline);
             printf("Pipeline: %s\n", pipeline_string);
 
-            pid_t pid = fork();
+            if (strncmp(pipeline_string, "cd ", 3) == 0)
+            {
+                // If the input starts with 'cd'
+                char *directory = pipeline_string + 3; // Extract directory path
+                remove_backslashes(directory);         // Remove backslashes
 
-            if (pid < 0)
-            {
-                perror("fork");
-                exit(1);
-            }
-            else if (pid == 0)
-            {
-                // Child process
-                execl("/bin/sh", "sh", "-c", pipeline_string, NULL);
-                perror("execl");
-                exit(1);
+                int change_dir_status = chdir(directory);
+                if (change_dir_status != 0)
+                {
+                    perror("chdir");
+                }
             }
             else
             {
-                // Parent process
-                int status;
-                waitpid(pid, &status, 0);
-                if (WIFEXITED(status))
+                pid_t pid = fork();
+
+                if (pid < 0)
                 {
-                    printf("Child process exited with status %d\n", WEXITSTATUS(status));
+                    perror("fork");
+                    exit(1);
+                }
+                else if (pid == 0)
+                {
+                    // Child process
+                    execl("/bin/sh", "sh", "-c", pipeline_string, NULL);
+                    perror("execl");
+                    exit(1);
+                }
+                else
+                {
+                    // Parent process
+                    int status;
+                    waitpid(pid, &status, 0);
+                    if (WIFEXITED(status))
+                    {
+                        printf("Child process exited with status %d\n", WEXITSTATUS(status));
+                    }
                 }
             }
         }
